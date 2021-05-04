28c921d56f8221e0da6433bff13d960969a743c3
Richard <richard10li2005@gmail.com>
2021-05-04 20:41:47 +0600

    changed allowed hosts

--- projpet/settings.py
+++ projpet/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '+-f-2ci(!p+@k(i=1_9xb9lz!2xpyc-lgwkv4szw4z^(q+_=ax'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
